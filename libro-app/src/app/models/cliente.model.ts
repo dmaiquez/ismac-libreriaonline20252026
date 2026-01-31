e88d38b9dc9845c06c212f9dc4503a9fc9fa4c27
dmaiquez <dmaiquez@tecnologicoismac.edu.ec>
2026-01-31 14:10:57 -0500

    cambio 2 en front end

--- libro-app/src/app/models/cliente.model.ts
+++ libro-app/src/app/models/cliente.model.ts
@@ -1,4 +1,6 @@
 export interface Cliente{
+    // comentarios para git hub desde front end
+    // comentarios para git hub desde front end 2
     idCliente: number
     cedula: string
     nombre: string
